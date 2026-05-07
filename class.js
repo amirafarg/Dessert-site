f94910aa55d5ed3200946a2e25be23479285f772
amirafarg <141867325+amirafarg@users.noreply.github.com>
2026-05-07 11:19:21 +0300

    dessert

--- class.js
+++ class.js
@@ -1,36 +1,59 @@
 class Cart {
   constructor() {
-    this.items = [];
+    this.item = [];
     this.totalPrice = 0;
   }
 
   // إضافة منتج
   addItem(product) {
-    this.items.push(product);
+    this.item.push(product);
     this.totalPrice += product.price;
   }
 
   // حذف منتج بالاسم
   removeItem(productName) {
-    const index = this.items.findIndex(item => item.name === productName);
+    const index = this.item.findIndex(item => item.name === productName);
     
     if (index !== -1) {
-      this.totalPrice -= this.items[index].price;
-      this.items.splice(index, 1);
+      this.totalPrice -= this.item[index].price;
+      this.item.splice(index, 1);
     }
   }
 
   // حساب الإجمالي من الأول
   calculateTotal() {
     this.totalPrice = 0;
-    this.items.forEach(item => {
+    this.item.forEach(item => {
       this.totalPrice += item.price;
     });
   }
 
   // عرض كل المنتجات
-//   displayItems() {
-//     console.log("Items in cart:");
-//     this.items.forEach(item =>) {
-//       console.log(${item.name} - $$…)}
+  displayItems() {
+    console.log("Items in cart:");
+    this.item.forEach(item => {
+      console.log(`${item.name} - $${item.price}`);
+    });
+  }
 }
+
+const cart = new Cart();
+
+// cart.addItem({ name: "waffle", price: 6.50});
+// cart.addItem({name: 'vanilla bean', price: 7.00});
+// cart.addItem({name: 'Macaron mix of five', price: 8.00});
+// cart.addItem({name: 'Classic tiramisu', price: 5.50});
+// cart.addItem({name: 'Pistachio baklava', price: 4.00});
+// cart.addItem({name: 'Lemon meringue', price: 5.00});
+// cart.addItem({name: 'Red velvet', price: 4.50});
+// cart.addItem({name: 'Salated caramel', price: 4.50});
+// cart.addItem({name: 'vanilla panna', price: 6.50});
+
+cart.displayItems();
+
+console.log("Total:", cart.totalPrice);
+
+cart.removeItem("vanilla bean");
+
+cart.displayItems();
+console.log("Total after remove:", cart.totalPrice);
--- js/app.js
+++ js/app.js
@@ -1,29 +1,182 @@
-const clickAdd = document.querySelector(".btn-cart");
-const show = document.querySelector(".alright");
-const added = document.querySelector(".abslt-add");
-const none = document.querySelector(".added-items");
-const card = document.getElementById("added-items");
+const cartTitle = document.querySelector(".added-items-js .storage");
+const addItemJs = document.querySelector(".added-items-js");
+const imptyCart = document.querySelector(".added-items");
 const btnConfirm = document.querySelector(".Confirm")
 const sec = document.querySelector(".sec");
 const popup = document.querySelector(".card-js");
 const removeCard = document.querySelector(".card");
 const getNewOrder = document.querySelector(".now-order");
-const btnPlus = document.querySelector(".plus");
 const plusInput = document.querySelector(".no-spinner");
+const addedDiv = document.getElementById("added");
+const totalOrder = document.querySelector(".number");
 
-clickAdd.addEventListener('click', clickToConvirt);
+// دالة تحديث لمجمل الاوردر
+function updateTotale() {
+    totalOrder.textContent = `$${cart.totalPrice.toFixed(2)}`;
+}
+
+const cardProduct = document.querySelectorAll(".img-relative");
+
+cardProduct.forEach(card => {
+    const addBtn = card.querySelector(".btn-cart");
+    const counter = card.querySelector(".abslt-add");
+    const btnPlus = card.querySelector(".plus");
+    const minusBtn = card.querySelector(".minus");
+    const input = card.querySelector("input");
+    const ok = card.querySelector(".alright");
+
+    addBtn.addEventListener("click", () => {
+        // const product = {
+        //     name: "Waffle",
+        //     price: 50
+        // };
+
+        // cart.addItem(product);
+
+        const name = card.parentElement.querySelector(".name-food").textContent;
+        const priceText = card.parentElement.querySelector(".price").textContent;
+
+        const price = Number(priceText.replace("$", ""));
+
+        // نخفي الزرار
+        addBtn.style.display = "none";
+
+        // نظهر العداد
+        counter.style.display = "flex";
+
+        // علامة الصح
+        ok.style.display = "block";
+
+        // كارت فاضي
+        imptyCart.classList.add("hide");
+
+        //  المضافه المنتجات ظهور كارت 
+        addItemJs.classList.add("show");
+
+        // اضافت المنتجات في الكارت
+        const item = document.createElement("div");
+        item.classList.add("first-order");
+
+        item.innerHTML = `
+        <div class="column">
+            <h6 class="vanilla">${name}</h6>
+            <h6 class="calc">
+                <span class="bold">1×</span> $${price} = $${price}
+            </h6>
+        </div>
+        <button class="btn-x">
+            <svg xmlns="http://www.w3.org/2000/svg" width="28" height="29" viewBox="0 0 28 29" fill="none">
+                <path
+                d="M11.375 11.875L16.625 17.125M16.625 11.875L11.375 17.125M24.5 14.5C24.5 15.8789 24.2284 17.2443 23.7007 18.5182C23.1731 19.7921 22.3996 20.9496 21.4246 21.9246C20.4496 22.8996 19.2921 23.6731 18.0182 24.2007C16.7443 24.7284 15.3789 25 14 25C12.6211 25 11.2557 24.7284 9.98182 24.2007C8.70791 23.6731 7.55039 22.8996 6.57538 21.9246C5.60036 20.9496 4.82694 19.7921 4.29926 18.5182C3.77159 17.2443 3.5 15.8789 3.5 14.5C3.5 11.7152 4.60625 9.04451 6.57538 7.07538C8.54451 5.10625 11.2152 4 14 4C16.7848 4 19.4555 5.10625 21.4246 7.07538C23.3938 9.04451 24.5 11.7152 24.5 14.5Z"
+                stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+            </svg>
+        </button>`;
+
+        const countEl = item.querySelector(".bold");
+        const calcText = item.querySelector(".calc");
+
+        addItemJs.appendChild(item);
+
+        input.value = 1;
+
+        //  إضافة المنتج للكارت
+        // addItemJs.prepend(item);
+        const title = addItemJs.querySelector(".storage");
+        title.insertAdjacentElement("afterend", item);
+
+        // زر الالغاء
+        const btnRemove = item.querySelector(".btn-x");
+        btnRemove.addEventListener("click", () => {
+            // مسح العنصر
+            item.remove();
+
+            // مسح من الcart
+            cart.removeItem(name);
+
+            // تحديث العدد من العنوان
+            title.textContent = `Your Cart (${cart.item.length})`;
+
+            // رجوع للزر الاصلي
+            addBtn.style.display = "flex";
+
+            // إخفاء العداد
+            counter.style.display = "none";
+
+            // إخفاء الصح
+            ok.style.display = "none";
+
+            // رجوع الinput
+            input.value = 1;
+
+            // لو مفيش منتجات الكارت يرجع
+            if (cart.item.length === 0) {
+                imptyCart.classList.remove("hide");
+                addItemJs.classList.remove("show");
+            }
+
+            updateTotale();
+        });
+
+        cart.addItem({ name, price });
+        updateTotale();
+
+        // عنوان الكارت وكميته
+        cartTitle.textContent = `Your Cart(${cart.item.length})`;
+
+        console.log(cart.items);
+
+        // زر ال+
+        btnPlus.addEventListener("click", () => {
+            let value = Number(input.value);
+            value++;
+
+            input.value = value;
+
+            cart.totalprice += 50;
+
+            // تحديث السعر
+            countEl.textContent = value;
+            calcText.innerHTML = `
+        <span class="bold">${value}</span>×
+        $${price} = $${(value * price).toFixed(2)}`
+
+            cart.totalPrice += price;
+            updateTotale()
+        });
+
+        // زر ال_
+        minusBtn.addEventListener('click', () => {
+            let value = Number(input.value);
+
+            if (value > 1) {
+                value--;
+                input.value = value;
+
+                cart.totalprice -= 50;
+
+                countEl.textContent = value;
+                calcText.innerHTML = `
+                <span class="count">${value}</span>× $${price} = $${(value * price).toFixed(2)}`;
+
+                art.totalPrice -= price;
+                updateTotale();
+            }
+        });
+    });
+
+});
+
 btnConfirm.addEventListener('click', clickToConfirm);
 removeCard.addEventListener('click', clickToRemove);
 getNewOrder.addEventListener('click', removeByBtn);
-btnPlus.addEventListener('click', increase);
 
 
 function clickToConvirt() {
-    clickAdd.classList.add('show');
-    show.classList.add('show');
-    added.classList.add('show');
-    none.classList.add('show');
-    card.classList.add('show');
+    // clickAdd.classList.add('show');
+    // show.classList.add('show');
+    // added.classList.add('show');
+    // none.classList.add('show');
+    // card.classList.add('show');
 }
 
 function clickToConfirm() {
